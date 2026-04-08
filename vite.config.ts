44341a555efcd3ec89ed72a82632564c30c8b825
Siam Sheikh <siam62349@gmail.com>
2026-04-08 08:34:42 +0600

    fix: simplify Vite configuration by removing unused environment loading and development proxy settings

--- vite.config.ts
+++ vite.config.ts
@@ -3,24 +3,14 @@ import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
+export default defineConfig(({ }) => {
 
   return {
-    base: "/",
     publicDir: "./public",
     plugins: [react(), tailwindcss(), tsconfigPaths()],
     server: {
       host: true,
       port: 5174,
-      ...(mode === "development" && {
-        proxy: {
-          "/api": {
-            target: env.VITE_SERVER,
-            changeOrigin: true,
-          },
-        },
-      }),
     },
     build: {
       target: "esnext",
